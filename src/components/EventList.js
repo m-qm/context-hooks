6f2fc47004857d03351dfb5c724b4d48c44c1194
m-qm <mireiaquerol@gmail.com>
2019-12-02 00:45:25 +0100

    feat: initial setup + navbar + eventlist

---
+++ src/components/EventList.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class EventList extends Component {
+  render () {
+    return (
+      <div className="event-list">
+        <ul>
+          <li>VH</li>
+          <li>HV</li>
+          <li>LR</li>
+
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default EventList;
